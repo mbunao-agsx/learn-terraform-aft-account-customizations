0ff6f8d2876d46334a7b789b0961386702a5d71a
mbunao-agsx <mbunao@agsx.net>
2022-09-27 00:10:06 +0800

    Added log group to sandbox

--- sandbox/terraform/log_group.tf
+++ sandbox/terraform/log_group.tf
@@ -1,10 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
-
 resource "aws_cloudwatch_log_group" "hello-world" {
   name = "hello-world"
   retention_in_days = 1
