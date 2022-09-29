7eb1dd99493c526277cce6a5b11b133fab007702
mbunao-agsx <mbunao@agsx.net>
2022-09-29 12:57:47 +0800

    Update

--- test/terraform/log_group.tf
+++ test/terraform/log_group.tf
@@ -1,11 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-test-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
 
 resource "aws_cloudwatch_log_group" "hello-world" {
-  name = "hello-world"
+  name = "hello-world-${data.aws_caller_identity.current.account_id}"
   retention_in_days = 1
 }
